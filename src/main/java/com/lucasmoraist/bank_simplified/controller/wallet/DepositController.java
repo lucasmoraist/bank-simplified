4d48b77933311400d5b5358cc860611a82644a60
lucasmoraist <luksmnt1101@gmail.com>
2024-09-05 19:27:17 -0300

    feat: Add DepositController for depositing money into a wallet

---
+++ src/main/java/com/lucasmoraist/bank_simplified/controller/wallet/DepositController.java
@@ -0,0 +1,42 @@
+package com.lucasmoraist.bank_simplified.controller.wallet;
+
+import com.lucasmoraist.bank_simplified.dto.DepositDTO;
+import com.lucasmoraist.bank_simplified.model.Wallet;
+import com.lucasmoraist.bank_simplified.service.WalletService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v2/wallet")
+@Tag(name = "V2")
+public class DepositController {
+
+    @Autowired
+    private WalletService service;
+
+    @Operation(summary = "Deposit", description = "Deposit money into a wallet")
+    @Parameter(name = "id", description = "Wallet's id", required = true)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deposit successful", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Wallet.class))),
+            @ApiResponse(responseCode = "404", description = "Wallet not found", content = @Content(mediaType = "application/json", schema = @Schema(example = """
+                    {
+                    	"message": "Wallet not found",
+                    	"status": "NOT_FOUND"
+                    }
+                    """))),
+    })
+    @PatchMapping("/{id}/deposit")
+    public ResponseEntity<Wallet> deposit(@PathVariable Long id, @RequestBody DepositDTO dto) {
+        Wallet wallet = this.service.deposit(id, dto);
+        return ResponseEntity.ok(wallet);
+    }
+
+}
