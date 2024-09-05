e06a215f3d52e6a096852655936d16e4431c2910
lucasmoraist <luksmnt1101@gmail.com>
2024-09-05 15:58:07 -0300

    feat: add UserController with savePerson and findById endpoints

---
+++ src/main/java/com/lucasmoraist/bank_simplified/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.lucasmoraist.bank_simplified.controller;
+
+import com.lucasmoraist.bank_simplified.dto.UserDTO;
+import com.lucasmoraist.bank_simplified.model.User;
+import com.lucasmoraist.bank_simplified.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> savePerson(@Valid @RequestBody UserDTO dto) {
+        User user = this.userService.save(dto);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = this.userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+}
