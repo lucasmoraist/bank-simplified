9d722f179366c54bf845e66217673111687a2dfb
lucasmoraist <luksmnt1101@gmail.com>
2024-09-05 15:59:55 -0300

    feat: Add AuthorizationValidation interface and implementation

---
+++ src/main/java/com/lucasmoraist/bank_simplified/validation/AuthorizationValidation.java
@@ -0,0 +1,5 @@
+package com.lucasmoraist.bank_simplified.validation;
+
+public interface AuthorizationValidation {
+    void validateAuthorization();
+}
---
+++ src/main/java/com/lucasmoraist/bank_simplified/validation/impl/AuthorizationValidationImpl.java
@@ -0,0 +1,24 @@
+package com.lucasmoraist.bank_simplified.validation.impl;
+
+import com.lucasmoraist.bank_simplified.exceptions.TransactionRoles;
+import com.lucasmoraist.bank_simplified.model.AuthorizationResponse;
+import com.lucasmoraist.bank_simplified.repository.AuthorizationClient;
+import com.lucasmoraist.bank_simplified.validation.AuthorizationValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationValidationImpl implements AuthorizationValidation {
+
+    @Autowired
+    private AuthorizationClient authorizationClient;
+
+    @Override
+    public void validateAuthorization() {
+        AuthorizationResponse authorizationResponse = this.authorizationClient.authorize();
+        if (authorizationResponse.getStatus().equals("fail") || !authorizationResponse.getData().isAuthorization()) {
+            throw new TransactionRoles("Transaction not authorized");
+        }
+    }
+
+}
