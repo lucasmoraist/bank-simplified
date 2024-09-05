a8c9ea29c27274c4beb21b0a873c33867108ca73
lucasmoraist <luksmnt1101@gmail.com>
2024-09-05 08:31:01 -0300

    first commit

---
+++ src/main/java/com/lucasmoraist/bank_simplified/BankSimplifiedApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.bank_simplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSimplifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmoraist/bank_simplified/BankSimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.bank_simplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
